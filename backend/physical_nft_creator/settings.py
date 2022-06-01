96580955b29f4c9e94f56fb0b14ce7355c3cfc9c
Bartosz Maślanka <56345225+maslana146@users.noreply.github.com>
2022-06-01 10:25:52 +0200

    Update settings.py

--- backend/physical_nft_creator/settings.py
+++ backend/physical_nft_creator/settings.py
@@ -178,10 +178,7 @@ LOGGING = {
 }
 # Email informations
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pnft.tokens@gmail.com'
-DEFAULT_FROM_EMAIL = 'pnft.tokens@gmail.com'
-EMAIL_HOST_PASSWORD = 'zaq1@WSX'
+
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
